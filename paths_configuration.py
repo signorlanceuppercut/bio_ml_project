0e4615b8be93066386c0898ea660b33d7f05cff2
signorlanceuppercut <marlepore@unisa.it>
2023-05-15 14:56:55 +0200

    versione lunedì 15/05

--- paths_configuration.py
+++ paths_configuration.py
@@ -3,6 +3,7 @@ class PathsConfiguration:
     def __init__(self, is_fuzzy):
         self.train_path = "resources//dataset//our_data_set_train_NODMA.xlsx"
         self.test_path = "resources//dataset//our_data_set_test_NODMA.xlsx"
+
         if is_fuzzy:
             self.train_path = "resources//dataset//our_fuzzy_data_set_train_NODMA.xlsx"
             self.test_path = "resources//dataset//our_fuzzy_data_set_test_NODMA.xlsx"
